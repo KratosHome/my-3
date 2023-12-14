5deaf4832c20926674c69823d012b76d67233fc7
Oleg Tkach <KratosHome101@gmail.com>
2023-12-14 18:31:34 +0200

    reqwest

--- src/components/WrapperRequest/WrapperRequest.tsx
+++ src/components/WrapperRequest/WrapperRequest.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import {fetchReviews} from "@/api/date";
+"use client"
+import React, { useEffect, useState } from 'react';
+import { fetchReviews } from "@/api/date";
 import TrustedBy from "@/components/TrustedBy/TrustedBy";
 
-export default async function WrapperRequest() {
-    const reviews = await fetchReviews()
+export default function WrapperRequest() {
+    const [reviews, setReviews] = useState<any>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await fetchReviews();
+                setReviews(data);
+            } catch (error) {
+                console.error("Error fetching reviews:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    if (!reviews) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <div>
