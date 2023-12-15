8df919945a1c34515bce3de2fad81a5791c2e008
Oleg Tkach <KratosHome101@gmail.com>
2023-12-15 12:52:57 +0200

    background-color

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -17,6 +17,9 @@ export default function Home() {
             <section id="about">
                 <AboutMe/>
             </section>
+            <section id="projects">
+                <SelectedProjects/>
+            </section>
         </main>
     )
 }
