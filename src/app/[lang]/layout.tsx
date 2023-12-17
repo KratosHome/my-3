a0332731ee15e501c850efb70fc330c2e4d9e429
Oleg Tkach <KratosHome101@gmail.com>
2023-12-17 11:50:04 +0200

    meta4

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -45,6 +45,19 @@ export default async function RootLayout({
 
     return (
         <html lang={params.lang === "en" ? "en" : "ua"}>
+        <Head>
+            <GoogleTagManager/>
+            <meta property="og:title"
+                  content="CodeCraftMaster - Your Guide to the World of Programming"/>
+            <meta property="og:description"
+                  content="Development of complex and interesting projects"/>
+            <meta property="og:image" content={"/logo.png"}/>
+            <meta property="og:url" content="codecraftmaster.com"/>
+            <meta property="og:type" content="website"/>
+
+            <meta property="og:site_name" content="codecraftmaster"/>
+            <meta property="og:locale" content={params.lang === "en" ? "en" : "ua"}/>
+        </Head>
         <ReduxProvider>
             <body className={`${JetBrainsMono.variable} ${consolas.variable} ${JetBrainsMonoBold.variable}`}>
             <NavBar navigation={navigation}/>
