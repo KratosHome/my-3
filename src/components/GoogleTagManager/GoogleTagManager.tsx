1d214475ac37dfa4c7643697603bf04017e6c81c
Oleg Tkach <KratosHome101@gmail.com>
2023-12-17 16:55:32 +0200

    8

--- src/components/GoogleTagManager/GoogleTagManager.tsx
+++ src/components/GoogleTagManager/GoogleTagManager.tsx
@@ -5,18 +5,23 @@ const GoogleTagManager = () => {
         const GTM_ID = `${process.env.NEXT_GTM_ID}`;
         return (
             <>
-                <script async src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`} />
-                <script dangerouslySetInnerHTML={{
-                    __html: `
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', '${GTM_ID}', {
-      page_path: window.location.pathname,
-    });
-  `,
-                }} />
-
+                <Script
+                    src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
+                    strategy="afterInteractive"
+                />
+                <Script
+                    id="google-analytics"
+                    strategy="afterInteractive"
+                >
+                    {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag() { dataLayer.push(arguments); }
+          gtag('js', new Date());
+          gtag('config', '${GTM_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+                </Script>
             </>
         )
     }
