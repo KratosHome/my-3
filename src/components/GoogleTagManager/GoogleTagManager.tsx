d62f8c626b196b66cfbdf316d3e63ec2f5d88c0e
Oleg Tkach <KratosHome101@gmail.com>
2023-12-17 16:49:35 +0200

    7

--- src/components/GoogleTagManager/GoogleTagManager.tsx
+++ src/components/GoogleTagManager/GoogleTagManager.tsx
@@ -4,18 +4,20 @@ import Script from 'next/script';
 const GoogleTagManager = () => {
         const GTM_ID = `${process.env.NEXT_GTM_ID}`;
         return (
-            <div className="container">
-                <script async src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}></script>
-                <script id="google-analytics">
-                    {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-          gtag('config', '${GTM_ID}');
-        `}
-                </script>
-            </div>
+            <>
+                <script async src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`} />
+                <script dangerouslySetInnerHTML={{
+                    __html: `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${GTM_ID}', {
+      page_path: window.location.pathname,
+    });
+  `,
+                }} />
+
+            </>
         )
     }
 ;
