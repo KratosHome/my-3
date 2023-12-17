5f4c3245f0efb5e431f14ecca79ae7587363ae4f
Oleg Tkach <KratosHome101@gmail.com>
2023-12-17 17:40:53 +0200

    teervdf313

--- src/components/GoogleTagManager/GoogleTagManager.tsx
+++ src/components/GoogleTagManager/GoogleTagManager.tsx
@@ -7,17 +7,22 @@ const GoogleTagManager = () => {
             <>
                 <Script
                     src={`https://www.googletagmanager.com/gtag/js?id=G-1VKMHQQYBM`}
+                    strategy="afterInteractive"
                 />
-                <Script id="google-analytics" >
-                    {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag() { dataLayer.push(arguments); }
-          gtag('js', new Date());
-          gtag('config', 'G-1VKMHQQYBM', {
-            page_path: window.location.pathname,
-          });
-        `}
-                </Script>
+                <Script
+                    id="google-analytics"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag() { dataLayer.push(arguments); }
+            gtag('js', new Date());
+            gtag('config', 'G-1VKMHQQYBM', {
+              page_path: window.location.pathname,
+            });
+          `,
+                    }}
+                />
             </>
         )
     }
