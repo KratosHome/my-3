b942c518a1ebfb9f763b654f7e9e553b733be18a
Oleg Tkach <KratosHome101@gmail.com>
2023-12-17 11:33:43 +0200

    bot submit3 test233

--- src/components/GoogleTagManager/GoogleTagManager.tsx
+++ src/components/GoogleTagManager/GoogleTagManager.tsx
@@ -3,7 +3,6 @@ import Script from 'next/script';
 
 const GoogleTagManager = () => {
         const GTM_ID = `${process.env.NEXT_GTM_ID}`;
-        console.log("GTM_ID", GTM_ID)
         return (
             <>
                 <Script src="https://www.googletagmanager.com/gtag/js?id=G-1VKMHQQYBM"/>
