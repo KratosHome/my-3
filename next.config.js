b5acdb670cd9b4890a631f7f8f7a59d453995460
Oleg Tkach <KratosHome101@gmail.com>
2023-12-17 11:51:32 +0200

    meta44

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
     sassOptions: {
@@ -11,6 +11,13 @@ module.exports = {
                 destination: '/en',
                 permanent: true,
             },
-        ]
+        ];
     },
-}
+    env: {
+        NEXT_GTM_ID: process.env.NEXT_GTM_ID,
+        TELEGRAM_BOT_CHAT_ID: process.env.TELEGRAM_BOT_CHAT_ID,
+        TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
+        // Додайте тут інші змінні середовища, якщо це необхідно
+    },
+    // Додаткові конфігурації та налаштування
+};
