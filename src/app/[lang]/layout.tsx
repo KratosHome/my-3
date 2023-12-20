6b1778232020a2af0081b81de1b830d79b0e0a42
Oleg Tkach <KratosHome101@gmail.com>
2023-12-20 12:18:30 +0200

    meta2

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -66,16 +66,6 @@ export default async function RootLayout({
 
     return (
         <html lang={params.lang === "en" ? "en" : "ua"}>
-        <Head>
-            <title>{openGraphMeta.title}</title>
-            <meta property="og:title" content={openGraphMeta.title}/>
-            <meta property="og:description" content={openGraphMeta.description}/>
-            <meta property="og:url" content={openGraphMeta.url}/>
-            <meta property="og:image" content={openGraphMeta.url + openGraphMeta.image}/>
-            <meta property="og:site_name" content={openGraphMeta.site_name}/>
-            <meta property="og:type" content={openGraphMeta.type}/>
-            <meta property="og:image" content={`https://codecraftmaster.com${openGraphMeta.image}`}/>
-        </Head>
         <GoogleTagManager/>
         <ReduxProvider>
             <body className={`${JetBrainsMono.variable} ${consolas.variable} ${JetBrainsMonoBold.variable}`}>
