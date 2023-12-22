38e9763314d9c1c2176ab561f881ebb1261bb6d7
Oleg Tkach <KratosHome101@gmail.com>
2023-12-22 09:32:24 +0200

    sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,18 +1,14 @@
-import { MetadataRoute } from 'next'
+import {MetadataRoute} from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
             url: 'https://codecraftmaster.com/ua',
             lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 1.0,
         },
         {
             url: 'https://codecraftmaster.com/en',
             lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 1.0,
         },
     ]
 }
