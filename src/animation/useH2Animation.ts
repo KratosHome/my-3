1ec8537b0ecccc682583a02a2c616969c9aeb166
Oleg Tkach <KratosHome101@gmail.com>
2024-02-11 16:09:04 +0200

    sum bags5

--- src/animation/useH2Animation.ts
+++ src/animation/useH2Animation.ts
@@ -1,34 +1,33 @@
-import { useRef, useEffect } from 'react';
+import {useRef} from 'react';
 import gsap from 'gsap';
+import {useGSAP} from "@gsap/react";
 
 
 export const useH2Animation = () => {
     const ref = useRef(null);
 
-    useEffect(() => {
+    useGSAP(() => {
         const element = ref.current;
 
-        if (element) {
-            gsap.fromTo(element,
-                {
-                    opacity: 0,
-                    scale: 0.5,
-                    y: -50
+        gsap.fromTo(element,
+            {
+                opacity: 0,
+                scale: 0.5,
+                y: -50
+            },
+            {
+                opacity: 1,
+                scale: 1,
+                y: 0,
+                duration: 1,
+                ease: "power3.out",
+                scrollTrigger: {
+                    trigger: element,
+                    start: "bottom bottom-=100",
+                    end: "top top-=10",
+                    toggleActions: "play reverse play reverse",
                 },
-                {
-                    opacity: 1,
-                    scale: 1,
-                    y: 0,
-                    duration: 1,
-                    ease: "power3.out",
-                    scrollTrigger: {
-                        trigger: element,
-                        start: "bottom bottom-=100",
-                        end: "top top-=10",
-                        toggleActions: "play reverse play reverse",
-                    },
-                });
-        }
+            });
     }, []);
 
     return ref;
