f67c5968c6c0797832298aaa11dd866bd314af85
Oleg Tkach <KratosHome101@gmail.com>
2024-03-18 08:32:38 +0200

    blog

---
+++ src/mokDate/blogDate.ts
@@ -0,0 +1,9 @@
+export const blogDate = [
+    {
+        id: 1,
+        titleEn: 'blog 1',
+        titleUa: 'blog 1',
+        descriptionUa: 'blog 1',
+        descriptionEn: 'blog 1',
+    }
+]
