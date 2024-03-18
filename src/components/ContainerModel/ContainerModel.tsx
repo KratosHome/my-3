befa4e3762c7d3111dd91a586f26460a82eaa9e6
Oleg Tkach <KratosHome101@gmail.com>
2024-03-18 15:40:40 +0200

    bagg4

--- src/components/ContainerModel/ContainerModel.tsx
+++ src/components/ContainerModel/ContainerModel.tsx
@@ -1,20 +1,10 @@
 "use client"
 import "./ContainerModel.scss";
 import Swim from "@/components/UIA/Swim/Swim";
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import ComputersCanvas from "@/components/ComputersCanvas/ComputersCanvas";
 
 const ContainerModel = () => {
-    const [scrollPosition, setScrollPosition] = useState(0);
-    const handleScroll = () => {
-        setScrollPosition(window.scrollY);
-    };
-
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
-
 
     return (
         <section className="main">
