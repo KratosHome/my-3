70c2ac353390e27c4da3a147d1a4147c08b5ae68
Oleg Tkach <KratosHome101@gmail.com>
2024-03-27 11:11:58 +0200

    add: pagination control

--- src/app/[lang]/admin/users/page.tsx
+++ src/app/[lang]/admin/users/page.tsx
@@ -3,12 +3,14 @@ import {redirect} from "next/navigation";
 import {getDictionary} from "@/services/dictionary";
 import {auth} from "@/lib/users/auth";
 import {getUsers} from "@/lib/users/userController";
+import PaginationControl from "@/components/PaginationControl/PaginationControl";
 
 export default async function Page({params: {lang}}: any) {
     const dict = await getDictionary(lang)
     const session: any = await auth();
 
     const getUsers2 = await getUsers(1, 10);
+    const totalPages = getUsers2.totalPages
 
     console.log(getUsers2);
 
@@ -18,6 +20,7 @@ export default async function Page({params: {lang}}: any) {
     return (
         <div>
             users
+            <PaginationControl totalPages={20}/>
         </div>
     );
 }
