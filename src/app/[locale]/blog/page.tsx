cd5ce0aed3e4a0df3b2336643bb7a9c74fe7cc26
Oleg Tkach <KratosHome101@gmail.com>
2024-04-08 12:30:14 +0300

    update: sum optomization7

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -1,4 +1,3 @@
-"use server"
 import "./blog.scss"
 import BlogList from "@/components/BlogList/BlogList";
 import {getPosts} from "@/server/post/postController";
@@ -7,17 +6,11 @@ import AnimatedPage from "@/components/animationTransition/AnimatedPage/Animated
 
 
 export default async function Page({params: {locale}, searchParams}: any) {
-    const page = searchParams["page"] ?? "1"
-    const posts = await getPosts(page, 10, locale)
-    const totalPages = posts.totalPages
+
 
     return (
         <>
-            <div className="blog__container">
-                {posts.data.map((item) =>
-                    <BlogList key={item.title} item={item}/>
-                )}
-            </div>
+vsfvfdv
         </>
     );
 }
