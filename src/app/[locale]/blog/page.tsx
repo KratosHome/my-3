c474b39429ddfe81638da458cd09b9b2ab758fff
Oleg Tkach <KratosHome101@gmail.com>
2024-04-08 12:34:02 +0300

    update: sum optomization8

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -6,11 +6,16 @@ import AnimatedPage from "@/components/animationTransition/AnimatedPage/Animated
 
 
 export default async function Page({params: {locale}, searchParams}: any) {
-
+    const posts = await getPosts(page, 10, locale)
+    const totalPages = posts.totalPages
 
     return (
-        <>
-vsfvfdv
-        </>
+        <AnimatedPage>
+            <div className="blog__container">
+                {posts.data.map((item) =>
+                    <BlogList key={item.title} item={item}/>
+                )}
+            </div>
+        </AnimatedPage>
     );
 }
